c461ce5a79bab03ef7d0c25642c67fb06a077185
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2023-10-02 14:31:34 -0400

    Refactor routes to create /api route

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+
+var usersRouter = require('./users');
+router.use('/users', usersRouter);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var apiRouter = require('./api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/api', apiRouter)
 
 module.exports = router;
