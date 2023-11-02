ac901b21118b00843ce7c2bb549707494eac55df
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2023-11-01 20:51:06 -0400

    Add hashed password to the user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,54 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto')
 
 var UserSchema = new mongoose.Schema({
   username: String,
   email: String,
+  hashed_password: {
+    type: String,
+    required: 'Password is required'
+  },
+  salt: String
 });
+
+UserSchema.virtual('password')
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password)
+    console.log(this.hashed_password)
+  })
+  .get(function () {
+    return this._password
+  })
+UserSchema.path('hashed_password').validate(function (v) {
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.');
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required');
+  }
+}, null);
+
+UserSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+  encryptPassword: function (password) {
+    if (!password) return ''
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+      console.log(err);
+      return ''
+    }
+  },
+  makeSalt: function () {
+    return Math.round((new Date().valueOf() * Math.random())) + ''
+  }
+}
 
 module.exports = mongoose.model('User', UserSchema)
