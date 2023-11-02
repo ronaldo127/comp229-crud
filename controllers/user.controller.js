a0cdc5a0f0cdc4bd126d3e4bf9e1d38956294fd1
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2023-11-01 20:33:31 -0400

    Add error handling

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,24 @@
 let UserModel = require('../models/user.model');
 
 module.exports.save = async (req, res) => {
-    const user = new UserModel(req.body);
-    let result = await user.save();
-    res.json(result)
+    try {
+        const user = new UserModel(req.body);
+        let result = await user.save();
+        res.json(result)
+    } catch (err) {
+        return res.status('400').json({
+            error: "Could not save user"
+        })
+    }
 }
 
 module.exports.find = async (req, res) => {
-    let result = await UserModel.find(req.params)
-    res.json(result)
+    try {
+        let result = await UserModel.find(req.params)
+        res.json(result)
+    } catch (err) {
+        return res.status('400').json({
+            error: "Could not find user"
+        })
+    }
 }
